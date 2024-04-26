fcaa28014484fb23d795464f2daef5fa15ea4c9a
Gavin APERANO MOULOUNGUI <gavin.aperano-mouloungui@ekino.com>
2024-04-26 11:04:23 +0200

    worked well with php

--- Main/Main/views.py
+++ Main/Main/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import subprocess
-import ctypes
+import json
+
 
 
 
@@ -13,24 +14,27 @@ def home(request):
 @csrf_exempt
 def execute_code(request):
     if request.method == 'POST':
-        language = request.POST.get('language')
-        code = request.POST.get('code')
+        try:
+            data = json.loads(request.body.decode('utf-8'))
+            language = data.get('language')
+            code = data.get('code')
 
-        if language == 'php':
-            # Exéc PHP avec subprocess
-            try:
-                output = subprocess.check_output(['php', '-r', code], universal_newlines=True)
-            except subprocess.CalledProcessError as e:
-                output = e.output
-        elif language == 'python':
-            try:
-                output = str(eval(code))
-            except Exception as e:
-                output = str(e)   
-        else:
-            output = "Langage non pris en charge"
+            if language == 'php':
+                # Exécutez le code PHP avec subprocess
+                try:
+                    output = subprocess.check_output(['php', '-r', code], universal_newlines=True)
+                except subprocess.CalledProcessError as e:
+                    output = e.output
+            elif language == 'python':
+                try:
+                    output = str(eval(code))
+                except Exception as e:
+                    output = str(e)
+            else:
+                output = "Langage non pris en charge"
 
-        return JsonResponse({'output': output})
-
+            return JsonResponse({'output': output})
+        except json.JSONDecodeError:
+            return JsonResponse({'error': 'Format JSON invalide'}, status=400)
 
     return JsonResponse({'error': 'Méthode de requête non autorisée'}, status=405)
