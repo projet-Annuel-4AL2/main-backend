6f4ae6c184b8d769633307f6b7aa7c976f840c0a
Gavin APERANO MOULOUNGUI <gavin.aperano-mouloungui@ekino.com>
2024-05-24 15:13:56 +0200

    api user create ok , update makefile

--- bytebelt/API/userApi/models.py
+++ bytebelt/API/userApi/models.py
@@ -6,9 +6,9 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     username = models.CharField(max_length=100 , unique=True)
-    email = models.EmailField(max_length=100)
+    email = models.EmailField(max_length=100 , unique=True)
     password = models.CharField(max_length=100)
-    is_active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=False)
     role = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- bytebelt/API/userApi/serializers.py
+++ bytebelt/API/userApi/serializers.py
@@ -4,8 +4,8 @@ from .models import CustomUser
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
-        fields = ['id', 'username', 'email', 'username', 'password', 'is_active' , 'created_at', 'updated_at' , 'role']
+        model = CustomUser
+        fields = ['id', 'username', 'email', 'username', 'password', 'created_at', 'updated_at' , 'role']
         
     def create(self, validated_data):
         user = CustomUser.objects.create_user(**validated_data)
