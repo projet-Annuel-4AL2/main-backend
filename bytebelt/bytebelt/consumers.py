afa3418f7bf1652e8bd8c302ce5aec4a98bd1d5d
AperanoGavin <audesandrine6@gmail.com>
2024-06-26 21:32:18 +0200

    update websocket service

--- bytebelt/bytebelt/consumers.py
+++ bytebelt/bytebelt/consumers.py
@@ -2,18 +2,24 @@ import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 class CodeSessionConsumer(AsyncWebsocketConsumer):
+    sessions = {}
+
     async def connect(self):
-        #recupère l'id du post dans l'url et crée un groupe de nom 'codesession_{post_id}'
         self.post_id = self.scope['url_route']['kwargs']['post_id']
         self.room_group_name = f'codesession_{self.post_id}'
 
-        #rejoint le groupe
         await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
-        
+
         await self.accept()
+        session_state = self.sessions.get(self.post_id, {'code': '', 'language': 'javascript'})
+        await self.send(text_data=json.dumps({
+            'type': 'init',
+            'code': session_state['code'],
+            'language': session_state.get('language', 'javascript') 
+        }))
 
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
@@ -27,6 +33,11 @@ class CodeSessionConsumer(AsyncWebsocketConsumer):
 
         if message_type == 'code_change':
             code = data['code']
+            # save the code in the session state
+            if self.post_id not in self.sessions:
+                self.sessions[self.post_id] = {}
+            self.sessions[self.post_id]['code'] = code
+
             await self.channel_layer.group_send(
                 self.room_group_name,
                 {
@@ -36,6 +47,11 @@ class CodeSessionConsumer(AsyncWebsocketConsumer):
             )
         elif message_type == 'language_change':
             language = data['language']
+            # save the language in the session state
+            if self.post_id not in self.sessions:
+                self.sessions[self.post_id] = {}
+            self.sessions[self.post_id]['language'] = language
+
             await self.channel_layer.group_send(
                 self.room_group_name,
                 {
