06a7dcfb241f06b95d98c65413308628bbf7375d
AperanoGavin <audesandrine6@gmail.com>
2024-06-30 15:42:18 +0200

    add data to the app start

--- bytebelt/start.sh
+++ bytebelt/start.sh
@@ -3,4 +3,5 @@ until python manage.py migrate; do
 echo "Migrations failed, retrying in 3 seconds..."
 sleep 3
 done
+python manage.py loaddata dump_data.json --ignorenonexistent
 daphne -b 0.0.0.0 -p 8000 bytebelt.asgi:application
