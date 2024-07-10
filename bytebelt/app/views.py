895b944fcb4bc094b384b92f4e00d2e64ec942eb
MarcMalha <marc.malha@beanstock.com>
2024-07-10 14:57:47 +0200

    Fix: handle case where there is no input file

--- bytebelt/app/views.py
+++ bytebelt/app/views.py
@@ -15,22 +15,29 @@ def execute_code(request):
     if request.method == 'POST':
         try:
             data = json.loads(request.body.decode('utf-8'))
-            language = data.get('language')
-            code = data.get('code')
-            file_data = data.get('inputFile')
+        except json.JSONDecodeError:
+            return JsonResponse({'error': 'Format JSON invalide'}, status=400)
 
-            try:
-                language_enum_value = Language.from_str(language)
-            except ValueError:
-                return JsonResponse(
-                    {'error': 'Either language name incorrect or language not yet implemented'},
-                    status=400
-                )
+        if not isinstance(data, dict):
+            return JsonResponse({'error': 'Request body should contain a JSON object'}, status=400)
 
-            runner = Runner(DockerImplementation())
-            with tempfile.NamedTemporaryFile('wt', suffix='.py', encoding='utf8') as file:
-                file.write(code)
-                file.flush()
+        language = data.get('language')
+        code = data.get('code')
+        file_data = data.get('inputFile')
+
+        try:
+            language_enum_value = Language.from_str(language)
+        except ValueError:
+            return JsonResponse(
+                {'error': 'Either language name incorrect or language not yet implemented'},
+                status=400
+            )
+
+        runner = Runner(DockerImplementation())
+        with tempfile.NamedTemporaryFile('wt', suffix='.py', encoding='utf8') as file:
+            file.write(code)
+            file.flush()
+            if file_data is not None:
                 with tempfile.NamedTemporaryFile('wt') as inputFile:
                     inputFile.write(file_data['content'])
                     temp_dir_name = os.path.dirname(inputFile.name)
@@ -38,10 +45,9 @@ def execute_code(request):
                     inputFile.name = os.path.join(temp_dir_name, file_data['name'])
                     inputFile.flush()
 
-                    exit_code, output = runner.execute_code(file.name, language_enum_value, inputFile.name)
+                    exit_code, output = runner.execute_single_container(file.name, language_enum_value, inputFile.name)
+            else:
+                exit_code, output = runner.execute_single_container(file.name, language_enum_value)
 
-            return JsonResponse({'exit_code': exit_code, 'output': output.decode()})
-        except json.JSONDecodeError:
-            return JsonResponse({'error': 'Format JSON invalide'}, status=400)
-
+        return JsonResponse({'exit_code': exit_code, 'output': output.decode()})
     return JsonResponse({'error': 'Unauthorized request method !@!'}, status=405)
